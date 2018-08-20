779bf3997ea45ce2dff24943e98e90041f4a482c
Glitch (efficacious-william) <none>
2018-08-20 08:56:41 +0000

    🎁😜 Checkpoint ./server.js:1588544/506

--- server.js
+++ server.js
@@ -50,11 +50,25 @@ const listener = app.listen(process.env.PORT || 3000, () => {
 
 
 var Schema=mongoose.Schema;
+
 var userSchema = new Schema({
   _id: {
   'type': String,
   'default': shortid.generate
   },
-  user : { type: String, required: true },
+  name : { type: String, required: true },
+  exercices :[{ type: Schema.Types.ObjectId, ref:'Exercice'}]
 });
 var User = mongoose.model('User', userSchema);
+
+var exerciceSchema = new Schema({
+  _id: {
+  'type': String,
+  'default': shortid.generate
+  },
+  user : { type: Schema.Types.ObjectId, ref:'User'},
+  description : { type: String, required: true },
+  duration:{ type: Number, required: true },
+  
+});
+var Exercice = mongoose.model('Exercice', userSchema);
